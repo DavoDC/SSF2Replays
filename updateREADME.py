cc6ef08fe485e340e55f5f9bdc7311abc9f3e07b
David <45155292+DavoDC@users.noreply.github.com>
2022-02-15 15:08:18 +0800

    Added README updater script - initial Done from E15 Also setup replay repo on E15 Windows, no new replays there

---
+++ updateREADME.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Name: Update README
+# Authors: David 
+# Last Update: 02 FEB 2022
+# Requirements: Python 3.8
+# Run using: python3 updateREADME.py
+
+import sys
+
+# Main function
+def main(arguments):
+
+    # Print
+    print("###### UPDATE README by David ######")
+
+    # Open file for overwriting
+    # TO DO
+
+    # Write lines that are always the same
+    # TO DO
+
+    # Count number of replays
+    # TO DO
+    
+    # Write replay count
+    # TO DO
+
+    # Close file
+    # TO DO
+
+
+# If file is run from the command line:
+if __name__ == '__main__':
+
+    # Run main function with arguments then exit
+    sys.exit(main(sys.argv[1:]))
