ba16c0b5a3a6938110b1b8d69f39c0f0e8a17060
David <skywalker_dc@live.com.au>
2022-02-24 17:35:11 +0800

    New replays from Wine Linux X230

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 My Super Smash Flash 2 (SSF2) replay files.
 Helps me synchronize my replays across multiple computers.
 
+Note: Often the dates on the replays are one day ahead for some reason.
+
 Run `updateREADME.py` to update the line below.
 
-### Replay Count = 3829 (as of 16/02/22)
+### Replay Count = 3861 (as of 24/02/22)
---
+++ runUpdater.sh
@@ -0,0 +1,63 @@
+
+### Update SSF2 Replays Repo Script
+
+# Start message
+clear
+echo -e "\n### Welcome to SSF2 Repo Updater"
+
+
+### Update README
+# Notify
+echo -e "\nUpdating README..."
+
+# Run script silently
+python3 updateREADME.py > NUL
+
+
+### Check new README
+# Notify
+echo -e "\nNew README:"
+
+# Print readme
+cat README.md
+
+# New line
+echo -e "\n"
+
+# Wait for check
+read -p "If the README looks correct, press any key to continue ..."
+
+
+### Check repo
+# Notify
+echo -e "\nChecking repo..."
+
+# New line
+echo -e "\n"
+
+# Check against remote quietly
+git fetch > NUL
+
+# Get first part status
+git status | head -n 3
+
+# Wait for check
+read -p "If [up to date with 'origin/main'], press any key to continue ..."
+
+
+### Upload to GitHub
+# Notify
+echo -e "\nUploading to GitHub..."
+
+# Add
+git add .
+
+# Commit
+git commit -m "New replays from Wine Linux X230"
+
+# Push
+git push
+
+
+### Finish message
+echo -e "\n\nFinished!"
