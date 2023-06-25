637104fb5855e0ab98b0d3a549ae63aa047a9247
David <45155292+DavoDC@users.noreply.github.com>
2023-06-25 15:13:28 +0800

    Wrote commitToGit.sh

--- Helper_Scripts/commitToGit.sh
+++ Helper_Scripts/commitToGit.sh
@@ -1,67 +1,69 @@
 #!/usr/bin/env bash
 
-# Detected operating system
+
+### Detect operating system, notifying if fails
 detected_os=""
-
-echo -e "\nHello from commit to git!"
-
-
-# ### Determine operating system
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#     detected_os="Linux X230"
-#     echo -e "\nOS Detected: Ubuntu"
-
-# elif [[ "$OSTYPE" == "msys" ]]; then
-#     detected_os="Windows LEGION"
-#     echo -e "\nOS Detected: Windows"
-
-# else
-#     echo -e "\nSystem Detection Error!"
-#     exit
-# fi
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    detected_os="Linux X230"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    detected_os="Windows LEGION"
+else
+    echo -e "\nOperating System Detection Error!"
+    exit 1
+fi
 
 
-
-# ### Check connection
-
-# # TODO
-
-
-
-# ### Check if repo is up-to-date
-# # Notify
-# echo -e "\nChecking repo..."
-
-# # New line
-# echo -e "\n"
-
-# # Check against remote quietly
-# git fetch > /dev/null
-
-# # Get first part status
-# git status | head -n 3
-
-# # Wait for check
-# echo -e "\n"
-# read -p "If [up to date with 'origin/main'], press any key to continue ..."
+### Fetch from remote quietly, notifying if fails
+fetch_output=$(git fetch origin 2>&1)
+fetch_exit_code=$?
+if [[ $fetch_exit_code -ne 0 ]]; then
+    echo -e "\nFailed to connect to GitHub!"
+    echo -e "$fetch_output"
+    exit 1
+fi
 
 
+### Check local repo against remote
+# Get the latest hashes of local and remote commits
+latest_local_commit_hash=$(git rev-parse HEAD)
+latest_remote_commit_hash=$(git rev-parse origin/main)
+# If they don't match
+if [ "$latest_local_commit_hash" != "$latest_remote_commit_hash" ]; then
+    # Notify, pull, and exit
+    echo -e "\nThe local repo is not up-to-date with the remote!"
+    echo -e "Updating from the remote and exiting. Please check results and try again.\n"
+    git pull
+    exit 1
+fi
 
-# ### Upload to GitHub
-# # Notify
-# echo -e "\nUploading to GitHub..."
 
-# # Add all files
-# echo -e "\n"
-# git add .
+### Check if local repo has uncommitted changes
+# Get porcelain git status output
+status_output=$(git status --porcelain)
+# If output empty
+if [[ -z "$status_output" ]]; then
+    # Notify
+    echo "\nThe local repo has no new changes!"
+    exit 1
+fi
 
-# # Commit with message
-# echo -e "\n"
-# git commit -m "$detected_os: <Player(s)>"
 
-# # Add commit description via text editor
-# git commit --amend
+### Upload to GitHub
+# Notify
+echo -e "\nRepo passed checks!"
+echo -e "\nStarting GitHub update..."
 
-# # Push to remote
-# echo -e "\n"
-# git push
+# Add all new files
+echo -e "\n"
+git add .
+
+# Commit with message
+echo -e "\n"
+git commit -m "$detected_os: <Player(s)>"
+
+# Add commit description via text editor
+git commit --amend
+
+# Push to remote
+echo -e "\n"
+git push
--- commitNewReplays.sh
+++ commitNewReplays.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-############ Update SSF2 Replays Repo Script ############
+############ SSF2 Replays Repo Updater ############
 
 
 ###### Global Variables
@@ -11,7 +11,7 @@ SCRIPT_FOLDER="Helper_Scripts"
 # README updater script path
 README_SCRIPT="$SCRIPT_FOLDER/updateREADME.py"
 
-# Constants for README script exit codes
+# README updater script exit codes
 README_SUCCESS=0
 README_ERROR=1
 README_UP_TO_DATE=2
@@ -25,7 +25,7 @@ COMMIT_SCRIPT="$SCRIPT_FOLDER/commitToGit.sh"
 
 # Clear terminal and print start message
 clear
-echo -e "\n### Welcome to SSF2 Repo Updater ###"
+echo -e "\n### SSF2 Replays Repo Updater ###"
 
 
 # Notify
@@ -34,20 +34,24 @@ echo -e "\n# Checking files..."
 
 # Run README script and capture its output
 readme_output=$(python3 "$README_SCRIPT") 
-python_exit_code=$?
+readme_exit_code=$?
 
 
 # Act based on README script exit code
-if [ $python_exit_code -eq $README_SUCCESS ]; then
+if [ $readme_exit_code -eq $README_SUCCESS ]; then
 
     # If README was updated, we assume replays need updating too.
+    # This works because we are comparing
+    # the current replay count to the count previously committed.
     echo -e "New files detected!"
+    
     echo -e "\n# Updated README replay count!"
     echo "$readme_output"
+
     echo -e "\n# Starting commit sequence!"
     ./$COMMIT_SCRIPT
 
-elif [ $python_exit_code -eq $README_UP_TO_DATE ]; then
+elif [ $readme_exit_code -eq $README_UP_TO_DATE ]; then
 
     # If README is up-to-date, we assume replays are too.
     echo -e "\nAlready up to date!"
