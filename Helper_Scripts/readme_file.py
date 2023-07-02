86ba791eb607e7675ed25e59957b2e82223c3729
David <45155292+DavoDC@users.noreply.github.com>
2023-07-02 13:56:04 +0800

    Update readme_file.py
    Made more understandable

--- Helper_Scripts/readme_file.py
+++ Helper_Scripts/readme_file.py
@@ -10,67 +10,72 @@ from replay_line import ReplayLine
 # Defines a class that represents the repo's README file
 class ReadmeFile:
     def __init__(self, readme_file_path):
+        # Content holder for new README content
+        new_content = []
 
-        # Create a new ReplayLine
-        new_replay_line = ReplayLine()
+        # Flags for tracking replay line status
+        replay_line_found = False
+        replay_line_updated = False
 
         # Open README file in read mode and read in lines
         with open(readme_file_path, "r") as readme_file:
             lines = readme_file.readlines()
 
-        # Process the lines to find and update the replay line, if needed
-        replay_line_updated, updated_content = self.process_lines(lines, new_replay_line)
+        # For each README line
+        for curLine in lines:
 
-        # If the replay line was found and updated
+            # If the line is a replay line
+            if ReplayLine.is_replay_line(curLine):
+
+                # Set found as true
+                replay_line_found = True
+
+                # Process line
+                processed_line = process_replay_line(curLine)
+
+                # If didn't exit, and line was definitely changed
+                if processed_line != curLine:
+
+                    # Set updated as true
+                    replay_line_updated = True
+
+                    # Replace current line
+                    curLine = processed_line
+
+            # Append line to content
+            new_content.append(curLine)
+
+        # If replay line was not found, notify and exit
+        if not replay_line_found:
+            handle_error("Replay line not found in README")
+
+        # If replay line was updated:
         if replay_line_updated:
 
-            # Open README file in write mode and write the updated content
+            # Open README file in write mode and write the new content to it
             with open(readme_file_path, "w") as readme_file:
-                readme_file.writelines(updated_content)
+                readme_file.writelines(new_content)
 
             # Notify about success and exit with a success code
             print_v("Successfully updated README!")
             sys.exit(SUCCESS)
-        else:
-            # Else if not found, notify and exit with an error code
-            handle_error("Replay line not found in README")
 
-    @staticmethod
-    def process_lines(lines, new_replay_line):
-        # Content holder for updated README content
-        updated_content = []
 
-        # Flag to track if the replay line was found and updated
-        replay_line_updated = False
+def process_replay_line(replay_line):
 
-        # Iterate over lines
-        for line in lines:
+    # Create ReplayLine with current repo state and date
+    new_replay_line = ReplayLine()
 
-            # If this is the replay line
-            if ReplayLine.is_replay_line(line):
+    # If the given replay line is equivalent
+    if ReplayLine(replay_line) == new_replay_line:
 
-                # If equivalent to new one
-                if ReplayLine(line) == new_replay_line:
+        # The replay line is already up-to-date
+        print_v("Generated new line: " + new_replay_line.to_string())
+        print_v("Replay line is already up to date!")
+        sys.exit(UP_TO_DATE)
+    else:
 
-                    # Notify and exit with an up-to-date code
-                    print_v("Generated new line: " + new_replay_line.to_string())
-                    print_v("Replay line is already up to date!")
-                    sys.exit(UP_TO_DATE)
-                else:
-                    # Else if the replay line needs updating:
-                    # Convert the new line to a string
-                    new_replay_line_str = new_replay_line.to_string()
-
-                    # Always print out the new line
-                    print("\nNew line: " + new_replay_line_str)
-
-                    # Append the new line to the content instead of the old line
-                    updated_content.append(new_replay_line_str + "\n")
-
-                    # Set the flag to indicate that the replay line is found and updated
-                    replay_line_updated = True
-            else:
-                # For normal lines, just append as-is
-                updated_content.append(line)
-
-        return replay_line_updated, updated_content
+        # Else if the line needs updating, print updated line and return
+        new_replay_line_str = new_replay_line.to_string()
+        print("\nNew line: " + new_replay_line_str)
+        return new_replay_line_str + "\n"
